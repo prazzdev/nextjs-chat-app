d78397b19932fee4095c44714006980e5953a4e7
prazzdev <prazzid31@gmail.com>
2024-05-08 13:11:16 +0700

    update authenticate.js

--- src/pages/api/authenticate.js
+++ src/pages/api/authenticate.js
@@ -3,13 +3,23 @@ import axios from "axios";
 export default async function handler(req, res) {
   const { username } = req.body;
   try {
-    const r = await axios.put(
+    const response = await axios.put(
       "https://api.chatengine.io/users/",
-      { username: username, secret: username, first_name: username },
+      { username, secret, first_name: firstName },
       { headers: { "private-key": process.env.PRIVATE_KEY } }
     );
-    return res.status(r.status).json(r.data);
-  } catch (e) {
-    return res.status(e.response.status).json(e.response.data);
+
+    if (response.status >= 200 && response.status < 300) {
+      return res.status(response.status).json(response.data);
+    } else {
+      console.error("Respon tidak terduga:", response.status, response.data);
+      return res
+        .status(500)
+        .json({ message: "Kesalahan memperbarui pengguna" });
+    }
+  } catch (error) {
+    console.error("Kesalahan:", error);
+    // Tangani error spesifik jika diperlukan
+    return res.status(500).json({ message: "Kesalahan memperbarui pengguna" });
   }
 }
